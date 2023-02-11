15f228408779b6a24f908ec73c7fb7c38fb58de9
GerCenturion <gersoncenturion@outlook.com.ar>
2023-02-11 20:53:14 -0300

    Ejercicios M03 completos

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -13,7 +13,8 @@ function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-   return str1 === str2
+   return str1.length === str2.length
+
 }
 
 function menosQueNoventa(num) {
